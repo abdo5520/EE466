33e315ec630a9503ca7c04b1de6d6a4a6899ce8c
Abdallah Abdallah <abdo.aast@gmail.com>
2022-03-17 02:42:53 -0400

    Update FIR_LowPassFilter.py

--- FIR_LowPassFilter.py
+++ FIR_LowPassFilter.py
@@ -19,3 +19,27 @@ H = np.fft.fftshift(H) # make 0 Hz in the center
 w = np.linspace(-sample_rate/2, sample_rate/2, len(H)) # x axis
 plt.plot(w, H, '.-')
 plt.show()
+
+
+# Shift the filter in frequency by multiplying by exp(j*2*pi*f0*t)
+f0 = 10e3 # amount we will shift
+Ts = 1.0/sample_rate # sample period
+t = np.arange(0.0, Ts*len(h), Ts) # time vector. args are (start, stop, step)
+exponential = np.exp(2j*np.pi*f0*t) # this is essentially a complex sine wave
+
+h_band_pass = h * exponential # do the shift
+
+# plot impulse response
+plt.figure('impulse')
+plt.plot(np.real(h_band_pass), '.-')
+plt.plot(np.imag(h_band_pass), '.-')
+plt.legend(['real', 'imag'], loc=1)
+
+# plot the frequency response
+H = np.abs(np.fft.fft(h_band_pass, 1024)) # take the 1024-point FFT and magnitude
+H = np.fft.fftshift(H) # make 0 Hz in the center
+w = np.linspace(-sample_rate/2, sample_rate/2, len(H)) # x axis
+plt.figure('freq')
+plt.plot(w, H, '.-')
+plt.xlabel('Frequency [Hz]')
+plt.show()
