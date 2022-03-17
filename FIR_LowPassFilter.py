dd065d4c5ec98ceabdb525346d3046595e699915
Abdallah Abdallah <abdo.aast@gmail.com>
2022-03-17 01:35:48 -0400

    Create FIR_LowPassFilter.py

---
+++ FIR_LowPassFilter.py
@@ -0,0 +1,14 @@
+import numpy as np
+from scipy import signal
+import matplotlib.pyplot as plt
+
+num_taps = 51 # it helps to use an odd number of taps
+cut_off = 3000 # Hz
+sample_rate = 32000 # Hz
+
+# create our low pass filter
+h = signal.firwin(num_taps, cut_off, nyq=sample_rate/2)
+
+# plot the impulse response
+plt.plot(h, '.-')
+plt.show()
